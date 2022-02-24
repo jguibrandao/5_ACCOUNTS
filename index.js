e47069a940d11d973e11ba89f7981e8bedb26bf2
jguibrandao <jguibrandao@gmail.com>
2022-02-24 02:12:57 -0300

    implementing account building functions

--- index.js
+++ index.js
@@ -17,10 +17,49 @@ function operation() {
             "Sacar",
             "Sair"
         ]
-    }]).then((anwser) => {
+    }]).then(anwser => {
         const action = anwser["action"]
 
-        console.log(action)
+        if(action == "Criar Conta") {
+            createAccount()
+        }
     })
     .catch((err) => console.log(err))
+}
+
+function createAccount() {
+    console.log(chalk.bgGreen.black("Obrigado por escolher o nosso banco!"))
+    console.log(chalk.green("Defina as opções da sua conta a seguir:"))
+
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: "accountName",
+            message: "Digite um nome para sua conta:"
+        }
+    ]).then(anwser => {
+        const accountName = anwser["accountName"]
+
+        console.info(accountName)
+
+        if(!fs.existsSync("accounts")) {
+            fs.mkdirSync("accounts")
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black("Nome de usuário já existente. Tente novamente"))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function (err) {
+            console.log(err)
+        })
+
+        console.log(chalk.green("Conta criada"))
+        operation()
+    }).catch(err => console.log(err))
 }
