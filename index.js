e6d4306be414b9af1543067f717a1311aada4cea
jguibrandao <jguibrandao@gmail.com>
2022-02-26 00:17:25 -0300

    refactoring and adding some stuff to main func

--- index.js
+++ index.js
@@ -1,65 +1,43 @@
 const inquirer = require("inquirer")
 const chalk = require("chalk")
 
-const fs = require("fs")
+const createAccount = require("./src/createAccount")
+const deposit = require("./src/deposit")
 
 operation()
 
+//main function
 function operation() {
-    inquirer.prompt([{
-        type: "list",
-        name: "action",
-        message: "O que você deseja fazer",
-        choices: [
-            "Criar Conta",
-            "Consultar Saldo",
-            "Depositar",
-            "Sacar",
-            "Sair"
-        ]
-    }]).then(anwser => {
-        const action = anwser["action"]
-
-        if(action == "Criar Conta") {
-            createAccount()
-        }
-    })
-    .catch((err) => console.log(err))
-}
-
-function createAccount() {
-    console.log(chalk.bgGreen.black("Obrigado por escolher o nosso banco!"))
-    console.log(chalk.green("Defina as opções da sua conta a seguir:"))
-
-    buildAccount()
-}
-
-function buildAccount() {
     inquirer.prompt([
         {
-            name: "accountName",
-            message: "Digite um nome para sua conta:"
+            type: "list",
+            name: "action",
+            message: "O que você deseja fazer",
+            choices: [
+                "Criar Conta",
+                "Consultar Saldo",
+                "Depositar",
+                "Sacar",
+                "Sair"
+            ]
         }
     ]).then(anwser => {
-        const accountName = anwser["accountName"]
+        const action = anwser["action"]
 
-        console.info(accountName)
+        if (action == "Criar Conta") {
+            createAccount()
+        } else if (action == "Consultar Saldo") {
 
-        if(!fs.existsSync("accounts")) {
-            fs.mkdirSync("accounts")
+        } else if (action == "Depositar") {
+            deposit()
+        } else if (action == "Sacar") {
+
+        } else if (action == "Sair") {
+            console.log(chalk.bgBlue.black("Obrigado por usar o Accounts!"))
+            process.exit
         }
-
-        if(fs.existsSync(`accounts/${accountName}.json`)) {
-            console.log(chalk.bgRed.black("Nome de usuário já existente. Tente novamente"))
-            buildAccount()
-            return
-        }
-
-        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function (err) {
-            console.log(err)
-        })
-
-        console.log(chalk.green("Conta criada"))
-        operation()
-    }).catch(err => console.log(err))
+    })
+        .catch((err) => console.log(err))
 }
+
+module.exports = operation
